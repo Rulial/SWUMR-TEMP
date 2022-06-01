c08eef055b7afc818e731910c9874ebb511a0f9d
Rulial Space <98750732+Rulial@users.noreply.github.com>
2022-06-01 17:43:12 +0000

    Update

--- src/App.js
+++ src/App.js
@@ -7,15 +7,16 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          \SWUMR 
+         <br></br> <code> pronounced </code> like summer
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://swumr.xyz"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          WHITE PAPER
         </a>
       </header>
     </div>
