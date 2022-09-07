8afe54f9673ce8762e0f574eacfdca1a193a6f0f
Rulial <98750732+Rulial@users.noreply.github.com>
2022-09-07 10:07:14 +0000

    Update

--- src/App.js
+++ src/App.js
@@ -5,28 +5,22 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo" href="https://path-integral.com" />
         <p>
-          SWUMR </p>
-         <code> \ pronounced like summer </code>
+          pronounced like summer
+          </p>
+         
         
         <p>
         <a
           className="App-link"
-          href="https://pathintegral.substack.com/p/swumr-pi0-protocol?sd=pf"
+          href="https://docs.path-integral.com/Ecosystem/SWUMR/overview"
           target="_blank"
           rel="noopener noreferrer"
         >
-          TECHNICAL PAPER
+          INTRODUCTION
         </a> </p>
-       <p> <a
-          className="App-link"
-          href="https://path-integral.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-        PI0
-        </a> </p>
+       
       </header>
     </div>
   );
